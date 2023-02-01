5272a8ca511b784419a1597830f1b876b34a8a84
Shyoy <86570990+Shyoy@users.noreply.github.com>
2023-02-01 18:05:19 +0200

    search bar added button

--- Frontend/my-app/src/components/NavArea/SearchBar/SearchBar.tsx
+++ Frontend/my-app/src/components/NavArea/SearchBar/SearchBar.tsx
@@ -22,19 +22,26 @@ const SearchBar = () => {
   
   return (
     <div className='SearchBar'  >
-      <div className='Search-input mt-3'>
-        <input  type="text" placeholder=' Search' onBlur={()=>setIsHidden(true)} onFocus={()=>setIsHidden(false)}  value={query} onChange={(e)=>{setQuery(e.target.value)}} />
-        <button onClick={()=> setQuery('')}>X</button>
-        <ul hidden={isHidden} >
-          <>
-          {products.filter((product)=>product.name.includes(query)).slice(0,4).map((prod, i)=>
-          <li key={i}  onMouseDown={()=> {setQuery(prod.name);console.log(prod.name +" "+ prod.id);}}>
-            <span>{prod.name}</span>
-            <img className="card-img" src={config.productImagesUrl+prod.image} alt={prod.name +" image"}/>
-          </li>)}</>
-          
-        </ul>
+      <div className='smart-input'>
+        <div className='Search-input mt-3'>
+          <input  type="text" className='form-control' placeholder=' Search' onBlur={()=>setIsHidden(true)} onFocus={()=>setIsHidden(false)}  value={query} onChange={(e)=>{setQuery(e.target.value)}} />
+          <button className='clear' onClick={()=> setQuery('')}>X</button>
+          <button className="material-symbols-outlined search-icon">search</button>
+        </div>
+      
+        <div className='search-output'>
+          <ul hidden={isHidden} >
+            <>
+            {products.filter((product)=>product.name.includes(query)).slice(0,4).map((prod, i)=>
+            <li key={i}  onMouseDown={()=> {setQuery(prod.name);console.log(prod.name +" "+ prod.id);}}>
+              <span>{prod.name}</span>
+              <img className="card-img" src={config.productImagesUrl+prod.image} alt={prod.name +" image"}/>
+            </li>)}</>
+            
+          </ul>
+          </div>
       </div>
+        
       {/* <button className='btn' onClick={()=>console.log('button') }> Enter</button> */}
      
     </div>
