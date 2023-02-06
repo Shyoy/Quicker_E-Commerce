fe3f68cc447c812a2c8dc7992cb222e4eaa6f089
Shyoy <86570990+Shyoy@users.noreply.github.com>
2023-02-06 16:41:08 +0200

    selectors for buttons

--- Frontend/my-app/src/components/NavArea/SearchBar/SearchBar.tsx
+++ Frontend/my-app/src/components/NavArea/SearchBar/SearchBar.tsx
@@ -8,35 +8,29 @@ const SearchBar = () => {
   const [isHidden, setIsHidden] = useState(true)
   const [query, setQuery] = useState('')
   const products = useAppSelector(selectProducts);
-  const handleInput = () => {
-    if (isHidden) {
-      setIsHidden(false)
-      console.log('HandleInput-Focus')
-    }
-    else{
-      
-      setIsHidden(true)
-      console.log('HandleInput-Blur')
-      
-    }}
+ 
   
   return (
     <div className='SearchBar'  >
       <div className='smart-input'>
         <div className='Search-input mt-3'>
           <input  type="text" className='form-control' placeholder=' Search' onBlur={()=>setIsHidden(true)} onFocus={()=>setIsHidden(false)}  value={query} onChange={(e)=>{setQuery(e.target.value)}} />
+          <button className="material-symbols-outlined search-icon">search</button>{/*TODO  add func to button*/}
+          {query.length > 0 &&
           <button className='clear' onClick={()=> setQuery('')}>X</button>
-          <button className="material-symbols-outlined search-icon">search</button>
+            }
         </div>
       
         <div className='search-output'>
           <ul hidden={isHidden} >
             <>
-            {products.filter((product)=>product.name.includes(query)).slice(0,4).map((prod, i)=>
-            <li key={i}  onMouseDown={()=> {setQuery(prod.name);console.log(prod.name +" "+ prod.id);}}>
-              <span>{prod.name}</span>
-              <img className="card-img" src={config.productImagesUrl+prod.image} alt={prod.name +" image"}/>
-            </li>)}</>
+              {products.filter((product)=>product.name.includes(query)).slice(0,4).map((prod, i)=>
+
+              <li key={i}  onMouseDown={()=> {setQuery(prod.name);console.log(prod.name +" "+ prod.id);}}>
+                <span>{prod.name}</span>
+                <img className="card-img" src={config.productImagesUrl+prod.image} alt={prod.name +" image"}/>
+              </li>)}
+            </>
             
           </ul>
           </div>
