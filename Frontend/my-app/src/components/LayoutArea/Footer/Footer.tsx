0d475f26e1334fc1b879ae123dceeb6b7c9e3376
Shyoy <86570990+Shyoy@users.noreply.github.com>
2023-03-02 22:20:18 +0200

    upd looks for footer

--- Frontend/my-app/src/components/LayoutArea/Footer/Footer.tsx
+++ Frontend/my-app/src/components/LayoutArea/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='Footer'>
-      Copyright 2021 Footer
+      <span className='small'>&copy; 2023 Copyright</span> - ShaYoy  
     </div>
   )
 }
