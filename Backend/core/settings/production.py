3ce53c8f5a3e131e9f0e75c88bdddc25f811c0b6
Shyoy <86570990+Shyoy@users.noreply.github.com>
2023-03-07 01:25:15 +0200

    upd static files

--- Backend/core/settings/production.py
+++ Backend/core/settings/production.py
@@ -34,4 +34,4 @@ DATABASES = {
 # Static
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
